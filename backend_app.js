78208737b7bd26c13b9c07266fe25d267a1b9d85
Owain <owaine1@live.com>
2017-12-04 16:31:16 +0800

    Faster, faster, you fool, you fool! 		-- Bill Cosby

--- backend_app.js
+++ backend_app.js
@@ -3,12 +3,23 @@ var backend_app = express();
 var bodyParser = require('body-parser');
 var routes = require('./routes/main_routes');
 var port = 3500;
+var mongoose = require('mongoose');
+mongoose.Promise = require('bluebird');
+var passport = require('passport');
+var flash = require('connect-flash');
 
 backend_app.set('view engine', 'ejs');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var session = require('express-session');
+
+var configDB = require('./config/database.js');
+
 backend_app.use(express.static('public'));
 backend_app.use(bodyParser.urlencoded({
     extended: true
 }));
+
 backend_app.use(bodyParser.json());
 backend_app.use(routes);
 
@@ -16,3 +27,27 @@ backend_app.listen(port, listening);
 function listening() {
     console.log('broadcasting on localhost:' + port);
 }
+
+app.use(express.static('public'));
+mongoose.connect(configDB.url, {
+    useMongoClient: true
+});
+
+require('./config/passport')(passport);
+
+app.use(cookieParser()); 
+app.use(bodyParser()); 
+app.set('view engine', 'ejs');
+app.use(session({
+    secret: 'myamazingapp'
+}));
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+app.use(flash()); // use connect-flash for flash messages stored in session
+
+// routes ======================================================================
+require('./app/routes/main_routes.js')(app, passport); // load our routes and pass in our app and fully configured passport
+
+// launch ======================================================================
+app.listen(port);
+console.log('The magic happens on port ' + port);
