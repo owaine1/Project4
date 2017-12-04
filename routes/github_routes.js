ee94490f575842551b0efa761698d103526463cf
Owain <owaine1@live.com>
2017-12-04 15:09:36 +0800

    It's not the inital skirt length, it's the upcreep.

--- routes/github_routes.js
+++ routes/github_routes.js
@@ -1,10 +1,39 @@
-// from https://github.com/jaredhanson/passport-github#authenticate-requests
-app.get('/auth/github',
-passport.authenticate('github'));
+function github_routes(app, passport) {
+    console.log('showing github routes');
+    app.get('/auth/github', passport.authenticate('github', {
+        scope: 'email'
+    }));
+    app.get('/auth/github/callback',
+        passport.authenticate('github', {
+            successRedirect: '/profile',
+            failureRedirect: '/'
+        }));
+    app.get('/connect/github', passport.authorize('github', {
+        scope: 'email'
+    }));
+    app.get('/connect/github/callback',
+        passport.authorize('github', {
+            successRedirect: '/profile',
+            failureRedirect: '/'
+        }));
+    // unlink don't think I want so will delete later
+    // app.get('/unlink/github', function (req, res) {
+    //     var user = req.user;
+    //     user.github.token = undefined;
+    //     user.save(function (err) {
+    //         res.redirect('/profile');
+    //     });
+    // });
+}
+module.exports = github_routes;
 
-app.get('/auth/github/callback', 
-passport.authenticate('github', { failureRedirect: '/login' }),
-function(req, res) {
-  // Successful authentication, redirect home.
-  res.redirect('/');
-});
+// from https://github.com/jaredhanson/passport-github#authenticate-requests
+// app.get('/auth/github',
+// passport.authenticate('github'));
+
+// app.get('/auth/github/callback', 
+// passport.authenticate('github', { failureRedirect: '/login' }),
+// function(req, res) {
+//   // Successful authentication, redirect home.
+//   res.redirect('/');
+// });
