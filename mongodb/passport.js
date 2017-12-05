f4127e94ee93603e9c0cd80b84280d636f9e165a
Owain <owaine1@live.com>
2017-12-05 11:33:46 +0800

    Adde parvum parvo manus acervus erit. 	[Add little to little and there will be a big pile.] 		-- Ovid

--- mongodb/passport.js
+++ mongodb/passport.js
@@ -1,17 +1,16 @@
 var passport = require('passport');
 var GitHubStrategy = require('passport-github2').Strategy;
 var creds = require('./creds.js');
-
 var git_creds = {
-  clientID: creds.client_id,
-  clientSecret: creds.client_secret,
-  callbackURL: creds.callback_url
+    clientID: creds.clientID,
+    clientSecret: creds.clientSecret,
+    callbackURL: creds.callback_url
 };
 
 function get_access(accessToken, refreshToken, profile, done) {
-  console.log('getting access');
-  console.log(profile);
-  return done();
+    console.log('getting access');
+    console.log(profile);
+    return done();
 }
 
 var git_strategy = new GitHubStrategy(git_creds, get_access);
