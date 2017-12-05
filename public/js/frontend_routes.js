deb48a4a24c7b0de3fea975fd0cd8ad92fb4acb2
Owain <owaine1@live.com>
2017-12-05 12:44:29 +0800

    How can you work when the system's so crowded?

--- public/js/frontend_routes.js
+++ public/js/frontend_routes.js
@@ -1,24 +1,52 @@
 
 function do_routes($routeProvider) {
-    console.log('doing FE routes');
+    console.log('doing frontend routes');
     $routeProvider.when(
-      '/login', {
-        templateUrl: 'partials/login.html',
-        controller: 'login_controller'
-      }
-    ).when(
-      '/', {
-        templateUrl: 'partials/home.html',
-        controller: 'home_controller'
-      }
-    ).when('/logged_in', {
-      templateUrl: 'partials/logged_in.html'
-    });
-  }
+        '/login', {
+            templateUrl: 'partials/login.html', // check is correct route?
+            controller: 'login_controller'
+        })
+        .when(
+        '/gists', {
+            templateUrl: 'views/gists.ejs',
+            controller: 'gists_controller'
+        })
+        .when(
+        '/', {
+            templateUrl: 'views/main-view.ejs',
+            controller: 'main-view_controller'
+        })
+        .when(
+        '/help', {
+            templateUrl: 'views/help.ejs',
+            controller: 'help_controller'
+        })
+        .when(
+        '/', {
+            templateUrl: 'views/main-view.ejs',
+            controller: 'main-view_controller'
+        })
+        .when(
+        '/repos-main-page', {
+            templateUrl: 'views/repos-view.ejs',
+            controller: 'repos-view_controller'
+        })
+        .when(
+        '/settings', {
+            templateUrl: 'views/settings.ejs',
+            controller: 'settings_controller'
+        })
+        .when(
+        '/signout', {
+            templateUrl: 'views/signout.ejs',
+            controller: 'signout_controller'
+        });
+}
 
-  gists.ejs
-  help.ejs
-  main-view.ejs
-  repos-main-page.ejs
-  settings.ejs
-  signout.ejs
+// Likely add or take away from this...
+// gists.ejs X
+// help.ejs X
+// main - view.ejs X
+// repos - view.ejs X
+// settings.ejs X
+// signout.ejs X
