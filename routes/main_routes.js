7921c9a0198c60713f149f07d58da320e22415a0
Owain <owaine1@live.com>
2017-12-06 14:45:18 +0800

    Question: Is it better to abide by the rules until they're changed or help speed the change by breaking them?

--- routes/main_routes.js
+++ routes/main_routes.js
@@ -11,7 +11,7 @@ router.get('/logout', do_logout);
 
 function do_homepage(req, res) {
     console.log('do homepage');
-    res.render('test');
+    res.render('home');
 }
 function do_settings(req, res) {
     console.log('doing settings');
